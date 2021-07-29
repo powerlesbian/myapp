99c44ce283e48f6c4dfd7289b1ca22c1a4f80f0d
Gopibkk <gopinath900@hotmail.com>
2021-07-29 08:24:54 +0700

    Removed unwanted codes.

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -31,22 +31,7 @@ const Todolist = () => {
     }
 
   return (
-    //  <>
-    //  <h1 className="h1">{ title }</h1>
-    //  {todos.map((todo, i) => (
-    //     <div className="todo" key={i} id= {i} onClick={removeTodo}>{todo.text}</div>
-    //   ))}
-    //   <form onSubmit={handleSubmit}>
-    //   <input
-    //     type="text"
-    //     className="input"
-    //     value={value}
-    //     placeholder="Add Todo..."
-    //     onChange={e => setValue(e.target.value)} />
-    // </form>
-    // </>
-
-    <div className="app">
+     <div className="app">
       <div className="todo-list" >
         <h1>My Todo List...</h1>
         {todos.map((todo, i) => (
@@ -55,46 +40,6 @@ const Todolist = () => {
         <TodoForm addTodo={addTodo} />
       </div>
     </div>
-
-      // <div class="container">
-      //   <div class="row">
-      //     <div class="col">
-      //           <div className="app">
-      //       <div className="todo-list" >
-      //         <h1>Yesterday's list...</h1>
-      //         {todos.map((todo, i) => (
-      //           <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
-      //         ))}
-      //         <TodoForm addTodo={addTodo} />
-      //       </div>
-      //     </div>
-      //     </div>
-      //     <div class="col">
-      //           <div className="app">
-      //       <div className="todo-list" >
-      //         <h1>Today's list...</h1>
-      //         {todos.map((todo, i) => (
-      //           <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
-      //         ))}
-      //         <TodoForm addTodo={addTodo} />
-      //       </div>
-      //     </div>
-      //     </div>
-      //     <div class="col">
-      //           <div className="app">
-      //       <div className="todo-list" >
-      //         <h1>Tomorrow's list...</h1>
-      //         {todos.map((todo, i) => (
-      //           <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
-      //         ))}
-      //         <TodoForm addTodo={addTodo} />
-      //       </div>
-      //     </div>
-      //     </div>
-      //   </div>
-      // </div>
-
-
   );
 }
 export default Todolist;
