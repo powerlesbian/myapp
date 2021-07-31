373fc5cbc17c17af3d5691204346393b7c0724c1
Gopibkk <gopinath900@hotmail.com>
2021-07-31 14:24:41 +0700

    Updated

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React from 'react';
 import './App.css';
 import Todolist from "./components/Todolist";
 import Lists from './components/Lists';
-
+import DisplayButton from './components/DisplayButton';
+import DisplayStock from './components/DisplayStock';
 
 const App = () => (
-   // <div>
-   // <Todolist />
-   // <Lists />
-   // </div>
 
-   <div class="container">
-      <div class="row">
-         <div class="col">
-            <Todolist />
+         <div>
+            <DisplayStock />            
          </div>
-         <div class="col">
-            <Lists />
-         </div>
-         <div class="col">
-            <h1>Under construction</h1> 
-         </div>
-      </div>
-   </div>
+  
+   // <div class="container">
+   //    <div class="row">
+   //       <div class="col">
+   //          <Todolist />
+   //       </div>
+   //       <div class="col">
+   //          <Lists />            
+   //       </div>
+   //       <div class="col">
+   //          {/* <h1>Under construction</h1>  */}
+   //          <DisplayButton />  
+   //       </div>
+   //    </div>
+   // </div>
       
    );
 
---
+++ src/components/DisplayStock.jsx
@@ -0,0 +1,50 @@
+import Button from 'react-bootstrap/Button'
+import { useState } from 'react';
+
+const DisplayStock = () => {
+
+    //inital stock
+    
+    const [stock, setStock] = useState(
+            [
+                { name: "apple", instock: 2 },
+                { name: "pineapple", instock: 3 },
+                { name: "pear", instock: 0 },
+                { name: "peach", instock: 3 },
+                { name: "orange", instock: 1 }
+            ]
+        );
+    
+        // Do shopping
+    const moveToCart = (e) => {
+        e.preventDefault();
+        let [name, num] = e.target.innerHTML.split(":");
+        let newStock = stock.map((item, index) => 
+                {
+                    if (num == 0) return item;
+                    if (item.name == name) item.instock--;                     
+                    return item;                
+                }
+            );
+            setStock(newStock);   
+        };
+
+       
+ return (
+     <div className = "app3">      
+        <h1>Our Stock</h1>
+         {stock.map((item, index) => 
+                {
+                return (
+                     <>
+                        <Button variant="primary" onClick={moveToCart} key={index.toString()}>{item.name}:{item.instock}</Button>{' '}
+                    </>
+                    )
+                }
+             )
+         }
+    </div>
+  );
+}
+
+export default DisplayStock;
