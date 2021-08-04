4eb4abac6f1c9966c11c987f8ac6f30ccd297eef
G Chao <81912588+powerlesbian@users.noreply.github.com>
2021-08-04 19:43:48 +0800

    added FetchAndRender

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import './App.css';
 // import ATMsimple from './components/ATMsimple';
 // import ATMadvance from './components/ATMadvance';
 // import ATMchoice from './components/ATMChoice';
-import ATMBasic from './components/ATMBasic';
+import FetchAndRender from './components/getData4.jsx';
 
 const App = () => (   
       <div class="container">
       <div class="row">
       <div class="col"></div>
       <div class="col-6">
-            <ATMBasic />
+            <FetchAndRender />
       </div>
       <div class="col"></div>
       </div> 
